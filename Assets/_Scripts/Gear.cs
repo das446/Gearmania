5e067e8320c880c71967ec0462f1a7bfbbc83200
nintendavid26 <nintendavid26@aol.com>
2018-05-08 21:10:19 -0400

    Init Commit

---
+++ Assets/_Scripts/Gear.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gear : MonoBehaviour {
+
+	float speed;
+	Rigidbody2D rb2d;
+	public bool grounded;
+	float jumpForce;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		MoveH();
+		if(Input.GetKeyDown(KeyCode.Space) && grounded){
+			Jump();
+		}
+
+	}
+
+	void MoveH(){
+		float x = Input.GetAxis("Horizontal");
+		rb2d.velocity=new Vector2(x*speed,rb2d.velocity.y);
+	}
+
+	void Jump(){
+		rb2d.AddForce(Vector3.up*jumpForce);
+	}
+}
---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField] float speed;
+	[SerializeField] Rigidbody2D rb2d;
+	[SerializeField] bool grounded;
+	[SerializeField] float jumpForce;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		MoveH();
+		if(Input.GetKeyDown(KeyCode.UpArrow) && grounded){
+			Jump();
+		}
+
+		
+
+	}
+
+	void MoveH(){
+		float x = Input.GetAxis("Horizontal");
+		rb2d.velocity=new Vector2(x*speed,rb2d.velocity.y);
+		if(x<0){
+			transform.eulerAngles=new Vector3(0,180,0);
+		}
+		else if(x>0){
+			transform.eulerAngles = Vector3.zero;
+		}
+	}
+
+	void Jump(){
+		rb2d.AddForce(Vector3.up*jumpForce);
+		grounded = false;
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		grounded = true;
+	}
+
+	// IEnumerator ThrowGear(){
+
+	// }
+
+
+}
