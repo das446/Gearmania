e4ab0e4e4dd5d1ea2cecd748ee05d37cc539d07f
nintendavid26 <nintendavid26@aol.com>
2018-05-29 16:35:11 -0400

    Make electric code clearer

--- Assets/_Scripts/Environment/ElectricGround.cs
+++ Assets/_Scripts/Environment/ElectricGround.cs
@@ -2,32 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElectricGround : Ground,IPuzzleObject {
+public class ElectricGround : Ground, IPuzzleObject {
 
-	bool deadly=true;
+	bool deadly = true;
 
-    public void TurnOff()
-    {
-        deadly = true;
+	public void TurnOff() {
+		deadly = true;
 		GetComponent<SpriteRenderer>().color = Color.red;
-		if(GetComponent<Collider2D>().IsTouching(Player.player.col)){
-			Player.player.Die();
-			this.PlaySound("Zaped");
+		if (GetComponent<Collider2D>().IsTouching(Player.player.col)) {
+			ElectrocutePlayer(Player.player);
 		}
-    }
+	}
 
-    public void TurnOn()
-    {
-        deadly = false;
+	public void TurnOn() {
+		deadly = false;
 		GetComponent<SpriteRenderer>().color = Color.white;
 
-    }
+	}
 
-    public override void OnCollision(Player p){
-		if (deadly){
-			p.Die();
-		}
-		else{
+	public void ElectrocutePlayer(Player p) {
+		p.Die();
+		this.PlaySound("Zaped");
+	}
+
+	public override void OnCollision(Player p) {
+		if (deadly) {
+			ElectrocutePlayer(p);
+		} else {
 			base.OnCollision(p);
 		}
 	}
