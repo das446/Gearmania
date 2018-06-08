2a005b87e1d05eb332c1350d8ca50a163f5f16d6
nintendavid26 <nintendavid26@aol.com>
2018-06-07 23:14:41 -0400

    Began prototype of parralax

--- Assets/_Scripts/Helpers/Parralax.cs
+++ Assets/_Scripts/Helpers/Parralax.cs
@@ -9,20 +9,23 @@ public class Parralax : MonoBehaviour {
 
 	public float nextX;
 
-	public float spawnD;
-	Player player;
-	// Use this for initialization
-	void Start () {
-		player = Player.player;
-	}
-	
+	public float spawnD = 20;
+
 	// Update is called once per frame
-	void Update () {
-		if (player.transform.position.x >= nextX){
-			//SpawnObject();
-			nextX+=spawnD;
+	void Update() {
+		if (Player.player.transform.position.x >= nextX) {
+			SpawnObject();
+			nextX += spawnD + Random.Range(-1f, 1f);
 		}
+		transform.position = new Vector3(-Player.player.transform.position.x * 1.5f, 0, 0);
+
+	}
+
+	void SpawnObject() {
+		GameObject g = objects.RandomItem();
+		float y = Random.Range(-5, 5);
+		Vector3 pos = new Vector3(-transform.position.x, y, 0);
+		g = Instantiate(g, pos, Quaternion.identity);
+		g.transform.parent=transform;
 	}
 }
-
-
