bcaed85c56b010d0dd427482f61b3ca63a32d641
nintendavid26 <nintendavid26@aol.com>
2018-06-07 20:03:09 -0400

    Added one time switch Began parralax

---
+++ Assets/_Scripts/Environment/PuzzleSwitchOneTime.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleSwitchOneTime : PuzzleSwitch {
+
+	private void OnTriggerExit2D(Collider2D other) {
+		
+	}
+}
---
+++ Assets/_Scripts/Helpers/Parralax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parralax : MonoBehaviour {
+
+	public GameObject[] objects;
+	public GameObject layer;
+
+	public float nextX;
+
+	public float spawnD;
+	Player player;
+	// Use this for initialization
+	void Start () {
+		player = Player.player;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.x >= nextX){
+			//SpawnObject();
+			nextX+=spawnD;
+		}
+	}
+}
+
+
