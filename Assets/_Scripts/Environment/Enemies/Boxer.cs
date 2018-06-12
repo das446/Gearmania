9eeae87a77a6980dc9c12357141bb7b7974ca0fd
nintendavid26 <nintendavid26@aol.com>
2018-06-12 16:49:56 -0400

    Added some animations Added exit

---
+++ Assets/_Scripts/Environment/Enemies/Boxer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boxer : EnemyRobot {
+
+	public bool hiding;
+	// Use this for initialization
+	public override void OnTrigger(Gear gear){
+		if(hiding){return;}
+		base.OnTrigger(gear);
+	}
+
+	public void Hide(){
+		hiding=true;
+	}
+
+	public void UnHide(){
+		hiding=false;
+	}
+}
