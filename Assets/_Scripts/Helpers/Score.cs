b612a9f03ff85a71b398a0702dc38f8528c32464
nintendavid26 <nintendavid26@aol.com>
2018-06-22 20:26:07 -0400

    Began adding score

---
+++ Assets/_Scripts/Helpers/Score.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+	int currentScore;
+	public Text scoreDisplay;
+	int pointsForBestTime=1000;
+	int enemiesDestroyed=0;
+
+
+	public void AddPoints(int amnt,bool enemy=false){
+		currentScore+=amnt;
+		if(enemy){
+			enemiesDestroyed++;
+		}
+	}
+/// <summary>
+/// 
+/// </summary>
+/// <param name="bestTime">In Seconds</param>
+/// <returns></returns>
+	public int CalculateFinalScore(int bestTime){
+		int t = currentScore+enemiesDestroyed*10;
+		int levelTime = Mathf.RoundToInt(Time.timeSinceLevelLoad);
+		if(levelTime<bestTime){
+			levelTime=bestTime;
+		}
+		t += pointsForBestTime*bestTime/levelTime;
+		return t;
+	}
+	
+
+}
